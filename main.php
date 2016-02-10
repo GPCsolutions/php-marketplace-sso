6a4f9e0a76830804b5df2a7c3f727ae053d262c4
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2016-02-10 22:20:12 +0100

    Validate domain property to allow marketplace publication

--- main.php
+++ main.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="google-site-verification" content="gnPXZrlfQU0J_qu3uPKOzorSsuhkyNGRooNJ7ceC8Zo" />
+	<title>Hello, world!</title>
+</head>
+<body>
 <?php
 
 echo 'Hello, world!';
+
+?>
+</body>
